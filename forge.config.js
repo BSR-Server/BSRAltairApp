7c70611dc5b7b6ed400552d457ca5e5c52be9a77
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2022-12-30 16:30:09 +0000

    feat: 🎉 initial commit

---
+++ forge.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    packagerConfig: {
+        icon: './src/icons/icon',
+    },
+    rebuildConfig: {},
+    makers: [
+        {
+            name: '@electron-forge/maker-squirrel',
+            config: {
+                iconUrl: 'https://www.bsrserver.org:8443/minio/frontend/common/logos/logo.ico',
+                setupIcon: './src/icons/icon.ico',
+            },
+        },
+        {
+            name: '@electron-forge/maker-dmg',
+            config: {
+                format: 'ULFO',
+                icon: './src/icons/icon.icns',
+            },
+        },
+    ],
+};
